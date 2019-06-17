e238fb804147749eaf9624b7cc6c6623b89ca360
lesly houndole <lesly.houndole@crem.ch>
2019-06-17 11:08:38 +0200

    test CM

--- cm/app/helper.py
+++ cm/app/helper.py
@@ -37,51 +37,26 @@ def create_zip_shapefiles(output_directory, shapefile):
     os.chdir(output_directory)
     # determine file name
     import sys
+    #modify the coding in fly for writing
     sys.getfilesystemencoding = lambda: 'UTF-8'
     filename = shapefile.replace(output_directory+'/', "")
-
-
-
-    print ("shafefile",shapefile)
     zip_file = filename.replace('.shp', '.zip')
     shp_file = filename
-    print ("shp_file",shp_file)
     dbf_file = filename.replace('.shp', '.dbf')
-    print ("dbf_file",dbf_file)
     prj_file = filename.replace('.shp', '.prj')
-    print ("prj_file",prj_file)
     shx_file = filename.replace('.shp', '.shx')
-    print ("shx_file",shx_file)
     zf = ZipFile(zip_file, 'w')
-    print ("zf",zf)
-
-    #try:
-    zf.write(dbf_file)
-    zf.write(prj_file)
-    zf.write(shx_file)
-    zf.write(shp_file)
-
-
-    #finally:
-    print ("zfclose")
-    zf.close()
-    print ("zip_file ",zip_file)
+    try:
+        zf.write(dbf_file)
+        zf.write(prj_file)
+        zf.write(shx_file)
+        zf.write(shp_file)
+    finally:
+        zf.close()
     return zip_file
 
-def zipdir(path, ziph):
-    # ziph is zipfile handle
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            ziph.write(os.path.join(root, file))
 
-def zip_directory(path_to_zip):
-    print ('path_to_zip ',path_to_zip)
-    filename = str(uuid.uuid4()) + '.zip'
 
-    zipf = zipfile.ZipFile(filename, 'w', zipfile.ZIP_DEFLATED)
-    zipdir(path_to_zip, zipf)
-    zipf.close()
-    return path_to_zip
 
 def generate_directory(tile_path):
     access_rights = 0o755
