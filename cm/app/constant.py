7ca4246fa952531d26f66a233e3936f23303ae8c
fallahnejad <fallahnejad@eeg.tuwien.ac.at>
2019-11-13 19:36:08 +0100

    correct the constant for the mip gap

--- cm/app/constant.py
+++ cm/app/constant.py
@@ -111,17 +111,17 @@ INPUTS_CALCULATION_MODULE = [
                'input_max': 8760,
                'cm_id': CM_ID
                },
-               {'input_name': 'MIP Gap - Smaller gap leads to higher accuracy at cost of higher time',
+               {'input_name': 'MIPGap*1e-2 (Smaller gap leads to higher accuracy at cost of higher time)',
                 'input_type': 'range',
                 'input_parameter_name': 'mip_gap',
-                'input_value': '0.08',
+                'input_value': '0.1',
                 'input_priority': '1',
                 'input_unit': ' ',
-                'input_min': '0.01',
-                'input_max': '0.1',
-                'input_step': '0.01',
+                'input_min': 0.01,
+                'input_max': 0.10,
+                'input_step': 0.01,
                 'cm_id': CM_ID
-                 },
+                 }
               ]
 
 
