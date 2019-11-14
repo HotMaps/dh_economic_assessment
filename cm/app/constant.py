adb8e52faedadba2cd568b9f523e91d257b1068e
fallahnejad <fallahnejad@eeg.tuwien.ac.at>
2019-11-14 20:29:32 +0100

    update the input value of MIPGAP

--- cm/app/constant.py
+++ cm/app/constant.py
@@ -111,10 +111,10 @@ INPUTS_CALCULATION_MODULE = [
                'input_max': 8760,
                'cm_id': CM_ID
                },
-               {'input_name': 'MIPGap (Smaller gap leads to higher accuracy at cost of higher time)',
+               {'input_name': 'MIPGap*1e-2 (Smaller gap leads to higher accuracy at cost of higher time)',
                 'input_type': 'range',
                 'input_parameter_name': 'mip_gap',
-                'input_value': '0.1',
+                'input_value': '1',
                 'input_priority': '1',
                 'input_unit': ' ',
                 #'input_min': 0.01,
